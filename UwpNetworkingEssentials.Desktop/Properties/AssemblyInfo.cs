14602c4fbd7fa57312574fc395b23820dd0bcf5d
Sven Vinkemeier <sven@vinkemeier.de>
2017-03-05 19:21:39 +0100

    Added "Desktop" project for .NET Framework/Win32 apps (e.g. for Project Centennial scenarios)

---
+++ UwpNetworkingEssentials.Desktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("UWP Networking Essentials (Desktop)")]
+[assembly: AssemblyDescription("Simple and lightweight networking (including RPC) for Universal Windows Platform apps")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sven Erik Vinkemeier")]
+[assembly: AssemblyProduct("UWP Networking Essentials")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("850abf67-2d1e-409a-8e79-6781c00dbe30")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: ComVisible(false)]
--- UwpNetworkingEssentials/Properties/AssemblyInfo.cs
+++ UwpNetworkingEssentials/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sven Erik Vinkemeier")]
 [assembly: AssemblyProduct("UWP Networking Essentials")]
-[assembly: AssemblyCopyright("Copyright ©  2016-2017")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
